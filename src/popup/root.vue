c2fbb6113773571ddb1fbd1982aca6929d58151c
stakada7 <souta.takada@stakada7.com>
2019-02-22 02:43:46 +0900

    add: get my tasks from my/tasks API

--- src/popup/root.vue
+++ src/popup/root.vue
@@ -1,17 +1,44 @@
 <template lang="pug">
     div
-      el-button(type="primary" @click="tab") New tab 
+      h1 MyTasks
+      section(v-if="errored")
+        p エラーです
+      section(v-else)
+        div(v-if="loading") Loading...
+        ul
+          li(v-for="task in tasks") {{ task.limit_time | moment }} : {{ task.body }}
 </template>
 <script>
+  import Axios from 'Axios'
+  import moment from 'moment'
+
   export default {
-    data: () => ({
-    }),
-    computed: { },
-    created () { },
-    mounted () { },
-    methods: {
-      tab () {
-        chrome.tabs.create({ url: 'pages/app.html' })
+    data: () => {
+      return {
+        tasks: null,
+        loading: true,
+        errored: false
+      }
+    },
+    mounted () {
+      Axios.get('https://api.chatwork.com/v2/my/tasks', {
+        headers: {'X-ChatWorkToken': ''},
+        params: {
+          assigned_by_account_id: '',
+          status: 'open'
+        }
+      }).then(response => {
+        this.tasks = response.data
+      }).catch(error => {
+        console.log(error)
+        this.errored = true
+      }).finally(() => {
+        this.loading = false
+      })
+    },
+    filters: {
+      moment: function (date) {
+        return moment.unix(date).format('YYYY/MM/DD')
       }
     }
   }
