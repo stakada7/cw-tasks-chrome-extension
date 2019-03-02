6d71ad0070719c6108e836805bea6472e10d7a12
stakada7 <souta.takada@stakada7.com>
2019-03-02 23:02:45 +0900

    add: local setting

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import root from './root.vue'
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 /* eslint-disable no-new */
 new Vue({
   el: '#root',
